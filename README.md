8fbeab7613cdc4508e7f0a7cf117168ab68e8fbc
Nilay Shah <nilay.shah525@gmail.com>
2015-02-01 10:19:47 -0500

    Everything again.

---
+++ README.md
@@ -0,0 +1,10 @@
+#SignWave
+As an elderly bedridden patient, its hard to keep track of everything. Nurses and doctors make lives easier. To help out, we made an app that uses [Myo](https://www.thalmic.com/en/myo/) gestures and allows patients to notify the nurses and/or doctor via [twilio](http://twilio.com).
+
+
+Logo made from [Hipster Logo Generator](http://www.hipsterlogogenerator.com) and scored a 5 out of 5 on the hipstometer. Yes, we did drop that much swag.
+
+
+
+
+[YOUTUBE DEMO](https://www.youtube.com/watch?v=0_ze-sBpMM0&feature=youtube_gdata_player) (Only one gesture)
---
+++ backend/server.js
@@ -0,0 +1,65 @@
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var express = require('express');
+var moment = require('moment');
+var path = require('path');
+var request = require('request');
+//require the Twilio module and create a REST client
+var client = require('twilio')('AC701c5089cab000c0049d169ded1a82e0', '96a9f6f51fc65e380f6392200a64f1ba');
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '/')));
+
+app.post('/twilio_endpoint', function(req, res) {
+	// var msg = req.params;
+	console.log(req.body.message);
+	var msg = req.body.message;
+
+	if(msg.toString() != "Mr. Fisher needs help"){
+		//Send an SMS text message
+		client.sendMessage({
+
+		    to:'+16478794447', // Any number Twilio can deliver to
+		    from: '+16476910630', // A number you bought from Twilio and can use for outbound communication
+		    body: msg // body of the SMS message
+
+		}, function(err, responseData) { //this function is executed when a response is received from Twilio
+
+		    if (!err) { // "err" is an error received during the request, if any
+
+		        // "responseData" is a JavaScript object containing data received from Twilio.
+		        // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+		        // http://www.twilio.com/docs/api/rest/sending-sms#example-1
+
+		        console.log(responseData.from); // outputs "+14506667788"
+		        console.log(responseData.body); // outputs "word to your mother."
+
+		    }
+		});
+	} else{
+		//Place a phone call, and respond with TwiML instructions from the given URL
+		client.makeCall({
+
+		    to:'+16478085434', // Any number Twilio can call
+		    // from : '',
+		    from: '+16476910630', // A number you bought from Twilio and can use for outbound communication
+		    url: 'http://rickshukla.me/hello.xml' // A URL that produces an XML document (TwiML) which contains instructions for the call
+
+		}, function(err, responseData) {
+
+		    //executed when the call has been initiated.
+		    // console.log(responseData.from); // outputs "+14506667788"
+		    console.log(err); // outputs "+14506667788"
+
+		});
+	}
+});
+
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
