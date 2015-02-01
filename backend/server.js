bb0fe1f5a0b2f7f6a59f9ac66a76769ff7cecc13
Nilay Shah <Nilay@user3-198-47.wireless.utoronto.ca>
2015-02-01 06:31:20 -0500

    Twilio: call or text, not both.

--- backend/server.js
+++ backend/server.js
@@ -18,41 +18,44 @@ app.post('/twilio_endpoint', function(req, res) {
 	// var msg = req.params;
 	console.log(req.body.message);
 	var msg = req.body.message;
-	//Send an SMS text message
-	client.sendMessage({
 
-	    to:'+16478794447', // Any number Twilio can deliver to
-	    from: '+16476910630', // A number you bought from Twilio and can use for outbound communication
-	    body: msg // body of the SMS message
+	if(msg.toString() != "Help me!"){
+		//Send an SMS text message
+		client.sendMessage({
 
-	}, function(err, responseData) { //this function is executed when a response is received from Twilio
+		    to:'+16478794447', // Any number Twilio can deliver to
+		    from: '+16476910630', // A number you bought from Twilio and can use for outbound communication
+		    body: msg // body of the SMS message
 
-	    if (!err) { // "err" is an error received during the request, if any
+		}, function(err, responseData) { //this function is executed when a response is received from Twilio
 
-	        // "responseData" is a JavaScript object containing data received from Twilio.
-	        // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
-	        // http://www.twilio.com/docs/api/rest/sending-sms#example-1
+		    if (!err) { // "err" is an error received during the request, if any
 
-	        console.log(responseData.from); // outputs "+14506667788"
-	        console.log(responseData.body); // outputs "word to your mother."
+		        // "responseData" is a JavaScript object containing data received from Twilio.
+		        // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+		        // http://www.twilio.com/docs/api/rest/sending-sms#example-1
 
-	    }
-	});
+		        console.log(responseData.from); // outputs "+14506667788"
+		        console.log(responseData.body); // outputs "word to your mother."
 
-	//Place a phone call, and respond with TwiML instructions from the given URL
-	client.makeCall({
+		    }
+		});
+	} else{
+		//Place a phone call, and respond with TwiML instructions from the given URL
+		client.makeCall({
 
-	    to:'+16478085434', // Any number Twilio can call
-	    from: '+16476910630', // A number you bought from Twilio and can use for outbound communication
-	    url: 'http://localhost:3000/hello.xml' // A URL that produces an XML document (TwiML) which contains instructions for the call
+		    to:'+16478085434', // Any number Twilio can call
+		    from: '+16476910630', // A number you bought from Twilio and can use for outbound communication
+		    url: 'http://localhost:3000/hello.xml' // A URL that produces an XML document (TwiML) which contains instructions for the call
 
-	}, function(err, responseData) {
+		}, function(err, responseData) {
 
-	    //executed when the call has been initiated.
-	    // console.log(responseData.from); // outputs "+14506667788"
-	    console.log(err); // outputs "+14506667788"
+		    //executed when the call has been initiated.
+		    // console.log(responseData.from); // outputs "+14506667788"
+		    console.log(err); // outputs "+14506667788"
 
-	});
+		});
+	}
 });
 
 
